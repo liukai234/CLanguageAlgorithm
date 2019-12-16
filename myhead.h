fb91ef1e5efb8db253f32c6fde6c8276a27aa6df
liukai234 <2679327337@qq.com>
2019-12-16 11:05:12 +0800

    first commit

---
+++ myhead.h
@@ -0,0 +1,21 @@
+/**
+ * @description: 头文件
+ * @file: myhead.h
+ * @author: LiuKai
+ * @ver: 1.0 2019/12/26
+ * @lastaurhot:LiuKai
+ **/
+#ifndef __MYHEAD_H__
+#define __MYHEAD_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <windows.h>
+#include <io.h>
+
+//前向声明块
+
+
+
+#endif
