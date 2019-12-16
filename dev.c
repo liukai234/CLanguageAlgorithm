5d8d9485b497a7f44d5351568d265a6156f76c3f
liukai234 <2679327337@qq.com>
2019-12-16 11:10:56 +0800

    update init

---
+++ dev.c
@@ -0,0 +1,21 @@
+/**
+ * @description: dev°æ±¾
+ * @file: dev.c
+ * @author: LiuKai
+ * @ver: 1.0 2019/12/26
+ * @lastchange: LiuKai
+ **/
+#include "myhead.h"
+
+
+/**
+ * @description: main
+ * @author: LiuKai
+ * @param: int args, char *argv[]
+ * @return: 0
+ * @ver: 1.0 2019/12/26
+ * @lastchange: LiuKai
+ */
+int main(int args, char *argv[]){
+    return 0;
+}
