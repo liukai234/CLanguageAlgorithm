5cdf9c29b822c568491c2d9cd9b589b748aaeaee
liukai234 <2679327337@qq.com>
2019-12-21 13:50:18 +0800

    update something

--- myhead.h
+++ myhead.h
@@ -2,7 +2,7 @@
  * @description: 头文件
  * @file: myhead.h
  * @author: LiuKai
- * @ver: 1.0 2019/12/26
+ * @ver: 2.0 2019/12/21
  * @lastaurhot:LiuKai
  **/
 #ifndef __MYHEAD_H__
@@ -16,7 +16,7 @@
 
 #define MAX_FILE_NAME 100 // 文件名的长度
 #define MAX_STRING 100
-#define MENU_SEL 3                                                 // 菜单长度宏
+#define MENU_SEL 10       // 菜单长度宏
 #define MAX_FILE_NUM 1000 // 最大文件数量
 
 /**
@@ -24,36 +24,36 @@
  * @author: liukai
  * @ver:1.0 2019/12/19
  * @lastchange: liukai
- */ 
+ */
 /*设置输出前景色*/
-#define PRINT_FONT_BLA  printf("\033[30m"); //黑色
-#define PRINT_FONT_RED  printf("\033[31m"); //红色
-#define PRINT_FONT_GRE  printf("\033[32m"); //绿色
-#define PRINT_FONT_YEL  printf("\033[33m"); //黄色
-#define PRINT_FONT_BLU  printf("\033[34m"); //蓝色
-#define PRINT_FONT_PUR  printf("\033[35m"); //紫色
-#define PRINT_FONT_CYA  printf("\033[36m"); //青色
-#define PRINT_FONT_WHI  printf("\033[37m"); //白色
-/*设置输出背景色*/ 
-#define PRINT_BACK_BLA  printf("\033[40m"); //黑色
-#define PRINT_BACK_RED  printf("\033[41m"); //红色
-#define PRINT_BACK_GRE  printf("\033[42m"); //绿色
-#define PRINT_BACK_YEL  printf("\033[43m"); //黄色
-#define PRINT_BACK_BLU  printf("\033[44m"); //蓝色
-#define PRINT_BACK_PUR  printf("\033[45m"); //紫色
-#define PRINT_BACK_CYA  printf("\033[46m"); //青色
-#define PRINT_BACK_WHI  printf("\033[47m"); //白色
+#define PRINT_FONT_BLA printf("\033[30m"); //黑色
+#define PRINT_FONT_RED printf("\033[31m"); //红色
+#define PRINT_FONT_GRE printf("\033[32m"); //绿色
+#define PRINT_FONT_YEL printf("\033[33m"); //黄色
+#define PRINT_FONT_BLU printf("\033[34m"); //蓝色
+#define PRINT_FONT_PUR printf("\033[35m"); //紫色
+#define PRINT_FONT_CYA printf("\033[36m"); //青色
+#define PRINT_FONT_WHI printf("\033[37m"); //白色
+/*设置输出背景色*/
+#define PRINT_BACK_BLA printf("\033[40m"); //黑色
+#define PRINT_BACK_RED printf("\033[41m"); //红色
+#define PRINT_BACK_GRE printf("\033[42m"); //绿色
+#define PRINT_BACK_YEL printf("\033[43m"); //黄色
+#define PRINT_BACK_BLU printf("\033[44m"); //蓝色
+#define PRINT_BACK_PUR printf("\033[45m"); //紫色
+#define PRINT_BACK_CYA printf("\033[46m"); //青色
+#define PRINT_BACK_WHI printf("\033[47m"); //白色
 /*输出属性设置*/
-#define PRINT_ATTR_REC  printf("\033[0m");  //重新设置属性到缺省设置 
-#define PRINT_ATTR_BOL  printf("\033[1m");  //设置粗体 
-#define PRINT_ATTR_LIG  printf("\033[2m");  //设置一半亮度(模拟彩色显示器的颜色) 
-#define PRINT_ATTR_LIN  printf("\033[4m");  //设置下划线(模拟彩色显示器的颜色) 
-#define PRINT_ATTR_GLI  printf("\033[5m");  //设置闪烁 
-#define PRINT_ATTR_REV  printf("\033[7m");  //设置反向图象 
-#define PRINT_ATTR_THI  printf("\033[22m"); //设置一般密度 
-#define PRINT_ATTR_ULIN  printf("\033[24m");//关闭下划线 
-#define PRINT_ATTR_UGLI  printf("\033[25m");//关闭闪烁 
-#define PRINT_ATTR_UREV  printf("\033[27m");//关闭反向图象
+#define PRINT_ATTR_REC printf("\033[0m");   //重新设置属性到缺省设置
+#define PRINT_ATTR_BOL printf("\033[1m");   //设置粗体
+#define PRINT_ATTR_LIG printf("\033[2m");   //设置一半亮度(模拟彩色显示器的颜色)
+#define PRINT_ATTR_LIN printf("\033[4m");   //设置下划线(模拟彩色显示器的颜色)
+#define PRINT_ATTR_GLI printf("\033[5m");   //设置闪烁
+#define PRINT_ATTR_REV printf("\033[7m");   //设置反向图象
+#define PRINT_ATTR_THI printf("\033[22m");  //设置一般密度
+#define PRINT_ATTR_ULIN printf("\033[24m"); //关闭下划线
+#define PRINT_ATTR_UGLI printf("\033[25m"); //关闭闪烁
+#define PRINT_ATTR_UREV printf("\033[27m"); //关闭反向图象
 
 /**
  * @description: 树结构体
@@ -64,6 +64,7 @@
 typedef struct info0
 {
     char name[MAX_STRING];
+    char father[MAX_STRING];
     int id;
     char sex[MAX_STRING];
     char age[MAX_STRING];
@@ -83,12 +84,15 @@ typedef struct linkStack0
     struct linkStack0 *next;
 } linkStack;
 
-char menuSel[MENU_SEL][MAX_FILE_NAME] = {"help", "input", "exit"}; // 菜单索引
+char menuSel[MENU_SEL][MAX_FILE_NAME] = {"help", "ls", "mk", "open", "close",
+                                         "idFindPerson", "nameFindPerson", "treeInput", "printTreeNode", "exit"}; // 菜单索引
 
 // 前向声明块
 void menuPrint();
 int ls(char *path);
 void mk(char *inputFileName);
+chbrotree *load(chbrotree *root, char *fileName);
+bool save(chbrotree *root, char *fileName);
 chbrotree *idFindPerson(chbrotree *root, int id);
 chbrotree *nameFindPerson(chbrotree *root, char *name);
 chbrotree *treeInput(chbrotree *root, info myinfo, char *relation, char *relationName);
